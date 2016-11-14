133180b3d44baf7e4be5fff97a549d34080d1460
Vijay Jadhav <vijayjadhav008@gmail.com>
2016-11-15 01:04:58 +0530

    picking up chicks added

---
+++ picking_up_chicks.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+int main(){
+    int C,N,K,B,T;
+    cin>>C;
+    while(C--){ //test cases
+       cin>>N>>K>>B>>T;
+       vector<int> X(N),V(N); //start positions and speeds
+       for(int i=0;i<N;i++) cin>>X[i];
+       for(int i=0;i<N;i++) cin>>V[i];
+       
+    }
+
+    return 0;
+}
